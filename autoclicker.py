8575ad62871215159fa6d360178801b9aa2d400e
JuanDiegoBohorquezBatlle <127441642+JuanDiegoBohorquezBatlle@users.noreply.github.com>
2023-03-09 20:41:17 +0100

    Spanish version.
    Esta es la versión en español  del programa.

---
+++ autoclicker.py
@@ -0,0 +1,24 @@
+from pynput.mouse import Button, Controller
+from pynput import keyboard
+import time
+
+mouse = Controller()
+loop = True
+print('\n=====================================================================')
+print('=======Auto Clicker; Desarrollo Juan Diego Bohorquez Batlle==========')
+print('=====================================================================\n')
+
+print("\n*Recuerda*: Para parar el programa pulsa la tecla q\n")
+
+eco = float(input("\n¿Cúal quieres que sea el intervalo entre clics medido en segundos?: "))
+def pulsa(tecla):
+	print('Se ha pulsado la tecla ' + str(tecla))
+	if tecla == keyboard.KeyCode.from_char('q'):
+		return False
+
+escuchador = keyboard.Listener(pulsa)
+escuchador.start()
+
+while escuchador.is_alive():
+        mouse.click(Button.left)
+        time.sleep(eco)
