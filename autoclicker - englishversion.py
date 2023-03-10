51271e059824f82c1b5595c46a2d0b8ea17045b3
Juan Diego Bohorquez Batlle <127441642+JuanDiegoBohorquezBatlle@users.noreply.github.com>
2023-03-10 22:05:26 +0100

    Bug correction
    When you introduce a value less than 0.001 seconds the cycle was repeated too fast and caused a slow operation of the tool, now it does not allow, ​​and if you enter these values the script will ask you again for a valid number.

--- autoclicker - englishversion.py
+++ autoclicker - englishversion.py
@@ -4,13 +4,21 @@ import time
 
 mouse = Controller()
 loop = True
+eco = 1
+
 print('\n=====================================================================')
 print('=======Auto Clicker; Developed Juan Diego Bohorquez Batlle==========')
 print('=====================================================================\n')
 
 print("\n*Remember*: To stop the program press the key q\n")
 
-eco = float(input("\nHow many clickcs for interval do you want, measured in seconds: "))
+while eco >= 0.001:
+	cop = float(input("\nHow many clickcs for interval do you want? Measured in seconds: "))
+
+	if cop >= 0.001:
+		eco = 0
+	else:
+		print("\n\nThe interval it is too small, please try again\n\n")
 def pulsa(tecla):
 	if tecla == keyboard.KeyCode.from_char('q'):
 		return False
@@ -20,4 +28,4 @@ escuchador.start()
 
 while escuchador.is_alive():
         mouse.click(Button.left)
-        time.sleep(eco)
+        time.sleep(cop)
