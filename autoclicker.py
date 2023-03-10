9aea7263270ede0e2c6cf33dfdbd0266e776c9d1
Juan Diego Bohorquez Batlle <127441642+JuanDiegoBohorquezBatlle@users.noreply.github.com>
2023-03-10 21:56:59 +0100

    Correción de bug
    Cuando introducias un valor menor  que 0.001 segundos el ciclo se repetia demasiado rápido y causaba un lento funcionamiento de la herramienta, ahora no permite esos valores y si los introduces te volvera a preguntar por un número válido.

--- autoclicker.py
+++ autoclicker.py
@@ -4,13 +4,20 @@ import time
 
 mouse = Controller()
 loop = True
+eco = 1
 print('\n=====================================================================')
 print('=======Auto Clicker; Desarrollo Juan Diego Bohorquez Batlle==========')
 print('=====================================================================\n')
 
 print("\n*Recuerda*: Para parar el programa pulsa la tecla q\n")
 
-eco = float(input("\n¿Cúal quieres que sea el intervalo entre clics? Medido en segundos: "))
+while eco >= 0.001:
+	cop = float(input("\n¿Cúal quieres que sea el intervalo entre clics medido en segundos?: "))
+	if cop >= 0.001:
+		eco = 0
+	else:
+		print("\n\nEl intervalo introducido es muy pequeño, porfavor introduce otro\n\n")
+
 def pulsa(tecla):
 	if tecla == keyboard.KeyCode.from_char('q'):
 		return False
@@ -20,4 +27,4 @@ escuchador.start()
 
 while escuchador.is_alive():
         mouse.click(Button.left)
-        time.sleep(eco)
+        time.sleep(cop)
