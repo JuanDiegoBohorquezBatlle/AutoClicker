fdfb47c4e5738c95aa84b5ff07827af44170015f
Juan Diego Bohorquez Batlle <127441642+JuanDiegoBohorquezBatlle@users.noreply.github.com>
2023-03-18 18:17:30 +0100

    New features
    The main change compared to the previous commit is that it now creates a configuration file in the path C:/Users/Public/ so as not to have to repeat the same parameters every time, it also adds the option to choose the interval between clicks measured in second or millisecond.

--- autoclicker - englishversion.py
+++ autoclicker - englishversion.py
@@ -1,10 +1,13 @@
+import time
+import os
+import sys
 from pynput.mouse import Button, Controller
 from pynput import keyboard
-import time
 
+
+RUTA = "C:/Users/Public/conf.py"
+LOOP = True
 mouse = Controller()
-loop = True
-eco = 1
 
 print('\n=====================================================================')
 print('=======Auto Clicker; Developed Juan Diego Bohorquez Batlle==========')
@@ -12,20 +15,54 @@ print('=====================================================================\n')
 
 print("\n*Remember*: To stop the program press the key q\n")
 
-while eco >= 0.001:
-	cop = float(input("\nHow many clickcs for interval do you want? Measured in seconds: "))
+T = int(input(
+    "\nIf you want use the previous adjusts press 1, if you want new adjust press 2: "))
 
-	if cop >= 0.001:
-		eco = 0
-	else:
-		print("\n\nThe interval it is too small, please try again\n\n")
+if T == 2:
+
+    print("\nDo you want the interval to be measured in seconds or in milliseconds?\n")
+    E = input("\For milliseconds write ms, for seconds write s: ")
+
+    A = E.upper()
+    list1 = ["S", "SEGUNDO", "SEGUNDOS", "SECONDS", "SECOND"]
+    list2 = ["MS", "MILISEGUNDO", "MILISEGUNDOS",
+             "MILISECOND", "MILISECONDS", "MILLISECONDS", "MILLISECOND"]
+
+    while LOOP:
+        if A in list1:
+            C = float(input(
+                "\nHow many clickcs for interval do you want? Measured in seconds: "))
+            LOOP = False
+            if C < 0.001:
+                C = 0.001
+        elif A in list2:
+            C = float(input(
+                "\nHow many clickcs for interval do you want? Measured in milliseconds: "))/1000
+            LOOP = False
+            if C < 0.001:
+                C = 0.001
+        else:
+            print(
+                "The time format introduced is incorrect, please try again")
+
+        CONT = f"C = {str(C)}"
+
+        with open(RUTA, "w", encoding="utf-8") as archivo:
+            archivo.write(CONT)
+
+if os.path.exists(RUTA):
+    sys.path.insert(0, "C:/Users/Public")
+    from conf import C
+
+
 def pulsa(tecla):
-	if tecla == keyboard.KeyCode.from_char('q'):
-		return False
+    if tecla == keyboard.KeyCode.from_char('q'):
+        return False
+
 
 escuchador = keyboard.Listener(pulsa)
 escuchador.start()
 
 while escuchador.is_alive():
-        mouse.click(Button.left)
-        time.sleep(cop)
+    mouse.click(Button.left)
+    time.sleep(C)
