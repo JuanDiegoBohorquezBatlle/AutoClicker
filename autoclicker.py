a3f27f9e3c8d62ee6fb4cafc4da721904f072289
Juan Diego Bohorquez Batlle <127441642+JuanDiegoBohorquezBatlle@users.noreply.github.com>
2023-03-18 17:49:33 +0100

    Nuevas funcionalidades
    El principal cambio respecto al commit anterior es que ahora crea un archivo de configuración en la ruta C:/Users/Public/ para no tener que introducir reiteradamente los mismos parámetros, tambien se añade la opción de elegir entre clics por segundo o clicks por milisegundo.

--- autoclicker.py
+++ autoclicker.py
@@ -1,30 +1,67 @@
+import time
+import os
+import sys
 from pynput.mouse import Button, Controller
 from pynput import keyboard
-import time
 
+
+RUTA = "C:/Users/Public/conf.py"
+LOOP = True
 mouse = Controller()
-loop = True
-eco = 1
+
 print('\n=====================================================================')
 print('=======Auto Clicker; Desarrollo Juan Diego Bohorquez Batlle==========')
 print('=====================================================================\n')
 
 print("\n*Recuerda*: Para parar el programa pulsa la tecla q\n")
+T = int(input(
+    "\nSi quieres cargar unos ajustes anteriores pulsa 1, si quieres usar unos nuevos pulsa 2: "))
 
-while eco >= 0.001:
-	cop = float(input("\n¿Cúal quieres que sea el intervalo entre clics medido en segundos?: "))
-	if cop >= 0.001:
-		eco = 0
-	else:
-		print("\n\nEl intervalo introducido es muy pequeño, porfavor introduce otro\n\n")
+if T == 2:
+
+    print("\nQuieres que el intervalo se mida en segundos o en milisegundos\n")
+    E = input("\nPara milisegundos escribe ms, para segundos escribe s: ")
+
+    A = E.upper()
+    list1 = ["S", "SEGUNDO", "SEGUNDOS", "SECONDS"]
+    list2 = ["MS", "MILISEGUNDO", "MILISEGUNDOS",
+             "MILISECONDS", "MILSEGUNDO", "MILSEGUNDO"]
+
+    while LOOP:
+        if A in list1:
+            C = float(input(
+                "\n¿Cúal quieres que sea el intervalo entre clics medido en segundos?: "))
+            LOOP = False
+            if C < 0.001:
+                C = 0.001
+        elif A in list2:
+            C = float(input(
+                "\n¿Cúal quieres que sea el intervalo entre clics medido en milisegundos?: "))/1000
+            LOOP = False
+            if C < 0.001:
+                C = 0.001
+        else:
+            print(
+                "El fromato de tiempo introducido es incorrecto, porfavor vuelva a intertarlo")
+
+        CONT = f"C = {str(C)}"
+
+        with open(RUTA, "w", encoding="utf-8") as archivo:
+            archivo.write(CONT)
+
+if os.path.exists(RUTA):
+    sys.path.insert(0, "C:/Users/Public")
+    from conf import C
+
 
 def pulsa(tecla):
-	if tecla == keyboard.KeyCode.from_char('q'):
-		return False
+    if tecla == keyboard.KeyCode.from_char('q'):
+        return False
+
 
 escuchador = keyboard.Listener(pulsa)
 escuchador.start()
 
 while escuchador.is_alive():
-        mouse.click(Button.left)
-        time.sleep(cop)
+    mouse.click(Button.left)
+    time.sleep(C)
